edf6395921d9343b8475cee262291fa15ae746ca
Abhishek456321 <abhishek.khadka.499@gmail.com>
2025-12-03 14:45:35 +0545

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
-import EmployeeList from "./components/EmployeeList";
-import EmployeeSatisfactory from "./components/EmployeeSatisfactory";
-import EmployeeStatus from "./components/EmployeeStatus";
-import Graph from "./components/Graph";
-import Greeting from "./components/Greeting";
-import Header from "./components/Header";
-import Schedule from "./components/Schedule";
-import Summary from "./components/Summary";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
-    <div className="w-screen  pt-[102px]  bg-linear-to-b from-blue-500/15 to-blue-500/5">
-      <div className="">
-        <Header></Header>
-        <Greeting></Greeting>
-        <Summary></Summary>
-      </div>
-      <div className="bg-white mx-10 mt-1 rounded-3xl">
-        <div className="flex pt-5 px-5 border-b border-black/10">
-          <Schedule></Schedule>
-          <Graph></Graph>
-          <EmployeeStatus></EmployeeStatus>
-        </div>
-        <div className=" mt-1 flex">
-          <EmployeeList></EmployeeList>
-          <EmployeeSatisfactory></EmployeeSatisfactory>
-        </div>
-      </div>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </>
   );
 };
 
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,33 @@
+import EmployeeList from "../components/EmployeeList";
+import EmployeeSatisfactory from "../components/EmployeeSatisfactory";
+import EmployeeStatus from "../components/EmployeeStatus";
+import Graph from "../components/Graph";
+import Greeting from "../components/Greeting";
+import Header from "../components/Header";
+import Schedule from "../components/Schedule";
+import Summary from "../components/Summary";
+
+const Dashboard = () => {
+  return (
+    <div className="w-screen  pt-[102px]  bg-linear-to-b from-blue-500/15 to-blue-500/5">
+      <div className="">
+        <Header></Header>
+        <Greeting></Greeting>
+        <Summary></Summary>
+      </div>
+      <div className="bg-white mx-10 mt-1 rounded-3xl">
+        <div className="flex pt-5 px-5 border-b border-black/10">
+          <Schedule></Schedule>
+          <Graph></Graph>
+          <EmployeeStatus></EmployeeStatus>
+        </div>
+        <div className=" mt-1 flex">
+          <EmployeeList></EmployeeList>
+          <EmployeeSatisfactory></EmployeeSatisfactory>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
